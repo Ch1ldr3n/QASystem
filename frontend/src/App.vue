d31cb656261e5a9ea6869b68b80c887af4c5a1c4
Nick Cao <nickcao@nichi.co>
2021-10-10 13:31:50 +0800

    frontend: reworked homepage with element plus components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,33 +1,17 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <img alt="Vue logo" src="./assets/fufeiwenda_pic1_tu.png">
-    <HelloWorld msg="付费问答系统"/>
-
+  <Homepage/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Homepage from './components/Homepage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Homepage
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 50px;
-}
-img {
-  margin-bottom: 50px;
-  height: 250px;
-  width: 500px;
-}
 </style>
---
+++ frontend/src/components/Homepage.vue
@@ -0,0 +1,80 @@
+<template>
+  <el-container>
+    <el-header>
+      <el-menu mode="horizontal">
+        <el-menu-item index="1">付费问答</el-menu-item>
+      </el-menu>
+    </el-header>
+    <el-main>
+      <el-row justify="center">
+        <el-image
+        style="height: 500px"
+        :src="image"
+        :fit="fit"
+        ></el-image>
+      </el-row>
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-card class="box-card">
+            <template #header>
+              <div class="card-header">
+                <span>海量题目</span>
+              </div>
+            </template>
+            <div>
+              计算机、电子、金融、医药<br>
+              面面俱到
+            </div>
+          </el-card>
+        </el-col>
+        <el-col :span="8">
+          <el-card class="box-card">
+            <template #header>
+              <div class="card-header">
+                <span>专业认知</span>
+              </div>
+            </template>
+            <div>
+              领域前沿学者<br>
+              客观解答
+            </div>
+          </el-card>
+        </el-col>
+        <el-col :span="8">
+          <el-card class="box-card">
+            <template #header>
+              <div class="card-header">
+                <span>安全可信</span>
+              </div>
+            </template>
+            <div>
+              第三方支付平台<br>
+              可信结算
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
+    </el-main>
+    <el-footer>
+    </el-footer>
+  </el-container>
+</template>
+
+<script>
+const image = require('../assets/fufeiwenda_pic1_tu.png')
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      image
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
