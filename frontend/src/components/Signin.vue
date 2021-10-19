599ac1f5b818bf8386a3481e0cd64d8702d72efd
qiuyijun50 <1004887615@qq.com>
2021-10-19 20:04:48 +0800

    将登陆密码设置为不可见模式

--- frontend/src/components/Signin.vue
+++ frontend/src/components/Signin.vue
@@ -1,84 +1,97 @@
 <template>
   <el-dialog
-        style="text-align: center"
-        title="登陆"
-        :show-close=false
-        width="30%">
+    style="text-align: center"
+    title="登陆"
+    :show-close="false"
+    width="30%"
+  >
     <el-form label-width="80px">
       <el-form-item label="用户名">
-        <el-input placeholder="username" v-model="state.username" autocomplete="off"></el-input>
-        <span v-if="state.username_valid===false" style="color: red">请输入合法用户名!</span>
+        <el-input
+          placeholder="username"
+          v-model="state.username"
+          autocomplete="off"
+        ></el-input>
+        <span v-if="state.username_valid === false" style="color: red"
+          >请输入合法用户名!</span
+        >
       </el-form-item>
     </el-form>
-	<el-form label-width="80px">
+    <el-form label-width="80px">
       <el-form-item label="密码">
-        <el-input placeholder="password" v-model="state.password" autocomplete="off"></el-input>
-        <span v-if="state.password_valid===false" style="color: red">请输入合法密码!</span>
+        <el-input
+          placeholder="password"
+          v-model="state.password"
+          autocomplete="off"
+          type="password"
+        ></el-input>
+        <span v-if="state.password_valid === false" style="color: red"
+          >请输入合法密码!</span
+        >
       </el-form-item>
     </el-form>
     <span class="dialog-footer">
       <el-button v-on:click="quit">取 消</el-button>
-      <el-button type="primary" v-on:click="signin"
-                  :disabled="!state.valid"
-                  >登 陆</el-button>
+      <el-button type="primary" v-on:click="signin" :disabled="!state.valid"
+        >登 陆</el-button
+      >
     </span>
   </el-dialog>
 </template>
 
 <script>
 export default {
-	name: "Signin",
-	props:{
-		dialogVisible: {
-			type: Boolean,
-			default: () => true
-		},
-		
-	},
-	data(){
-		return {
-			state: {
-				username: "",
-				username_valid: true,
-				password: "",
-				password_valid: true,
-				valid: false,
-			}
-		}
-	},
-	methods: {
-		signin: function(){
-			this.$parent.signin_()//this.state.username,this.state.password),
-			this.state.username="",
-			this.state.password=""
-			},
-		quit: function(){
-			this.$parent.quit(),
-			this.state.username="",
-			this.state.password=""
-		},
-	},
-	watch: {
-		"state.username": {
-			handler(newName) {
-				this.state.username_valid = /^[A-Za-z\u4e00-\u9fa5][-A-Za-z0-9\u4e00-\u9fa5_]*$/.test(newName),
-				this.state.valid = this.state.username_valid&&this.state.password_valid;
-				if(newName=="")
-					this.state.username_valid = true;
-				if(this.state.username==""||this.state.password=="")
-					this.state.valid = false;
-			}
-		},
-		"state.password": {
-			handler(newName) {
-				this.state.password_valid = /^[A-Za-z][-A-Za-z0-9_]*$/.test(newName),
-				this.state.valid = this.state.username_valid&&this.state.password_valid;
-				if(newName=="")
-					this.state.password_valid = true;
-				if(this.state.username==""||this.state.password=="")
-					this.state.valid = false;
-			}
-		}
-	}
-}
+  name: "Signin",
+  props: {
+    dialogVisible: {
+      type: Boolean,
+      default: () => true,
+    },
+  },
+  data() {
+    return {
+      state: {
+        username: "",
+        username_valid: true,
+        password: "",
+        password_valid: true,
+        valid: false,
+      },
+    };
+  },
+  methods: {
+    signin: function () {
+      this.$parent.signin_(); //this.state.username,this.state.password),
+      (this.state.username = ""), (this.state.password = "");
+    },
+    quit: function () {
+      this.$parent.quit(),
+        (this.state.username = ""),
+        (this.state.password = "");
+    },
+  },
+  watch: {
+    "state.username": {
+      handler(newName) {
+        (this.state.username_valid =
+          /^[A-Za-z\u4e00-\u9fa5][-A-Za-z0-9\u4e00-\u9fa5_]*$/.test(newName)),
+          (this.state.valid =
+            this.state.username_valid && this.state.password_valid);
+        if (newName == "") this.state.username_valid = true;
+        if (this.state.username == "" || this.state.password == "")
+          this.state.valid = false;
+      },
+    },
+    "state.password": {
+      handler(newName) {
+        (this.state.password_valid = /^[A-Za-z][-A-Za-z0-9_]*$/.test(newName)),
+          (this.state.valid =
+            this.state.username_valid && this.state.password_valid);
+        if (newName == "") this.state.password_valid = true;
+        if (this.state.username == "" || this.state.password == "")
+          this.state.valid = false;
+      },
+    },
+  },
+};
 </script>
