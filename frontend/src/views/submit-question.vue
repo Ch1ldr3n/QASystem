0a6a21519fc768c6677a5b9ae68a843cea990df4
cxjcxggg <chexj18@163.com>
2021-10-26 17:57:05 +0800

    #12 modify submit-question page

--- frontend/src/views/submit-question.vue
+++ frontend/src/views/submit-question.vue
@@ -1,26 +1,23 @@
 <template>
   <div>
-    <el-container>
-      <el-aside width="400px" style="background-color: rgb(238, 241, 246)">
-        <el-image
-          style="width: 100; height: 100"
-          :src="require('../assets/logo.png')"
-          :fit="fill"
-        >
-        </el-image>
-        <h1>name</h1>
-        <p>descriptiondescriptiondescriptiondescription</p>
-        <p>
-          descriptiondescriptiondesc
-        </p>
-        <p>descriptiondescriptiondescription</p>
-        <p>descriptiondescriptiondescription</p>
-        <p>descriptiondescriptiondescription</p>
-        <p>descriptiondescriptiondescription</p>
-        <p>descriptiondescriptiondescription</p>
-        <p>descriptiondescriptiondescription</p>
-      </el-aside>
-      <el-main>
+    <el-main>
+      <el-container>
+        <el-aside width="unset">
+          <el-card class="box-card">
+            <div class="card-header">
+              <el-image :src="'https://cube.elemecdn.com/9/c2/f0ee8a3c7c9638a54940382568c9dpng.png'"/>
+            </div>
+            <div>
+              <el-descriptions direction="vertical" column="1">
+                <el-descriptions-item label="用户名">测试用户</el-descriptions-item>
+                <el-descriptions-item label="手机号">18100000000</el-descriptions-item>
+                <el-descriptions-item label="称号"><el-tag size="small">回答者</el-tag></el-descriptions-item>
+                <el-descriptions-item label="余额">999999.99</el-descriptions-item>
+              </el-descriptions>
+            </div>
+          </el-card>
+        </el-aside>
+        <el-container>
         <el-form label-width="120px">
           <el-form-item label="问题名称">
             <el-input v-model="qname"></el-input>
@@ -33,8 +30,9 @@
             <el-button>取消提问</el-button>
           </el-form-item>
         </el-form>
+        </el-container>
+      </el-container>
       </el-main>
-    </el-container>
   </div>
 </template>
 
