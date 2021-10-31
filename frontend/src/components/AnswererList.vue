25a6d290739f4979cef861fe33982821049af4cb
zhoumanying <my-zhou19@mains.tsinghua.edu.cn>
2021-10-31 09:39:36 +0800

    Get the list of respondents from the back end

--- frontend/src/components/AnswererList.vue
+++ frontend/src/components/AnswererList.vue
@@ -1,8 +1,8 @@
 <template>
     <el-table
     ref="filterTable"
-    :data="tableData"
-    :default-sort="{ prop: 'name', order: 'descending' }"
+    :data="userlist"
+    :default-sort="{ prop: 'username', order: 'descending' }"
     style="width: 100%"
   >
     <el-table-column type="expand">
@@ -11,12 +11,11 @@
             <el-button @click="ask">向他提问</el-button>
       </template>
     </el-table-column>
-    <el-table-column prop="name" label="姓名" width="180" column-key="name"/>
-    <el-table-column prop="date" label="注册日期" />
-    <el-table-column prop="money" label="价格" sortable width="180" />
+    <el-table-column prop="username" label="姓名" width="180" column-key="name"/>
+    <el-table-column prop="price" label="价格" sortable width="180" />
 
     <el-table-column
-      prop="area"
+      prop="profession"
       label="专业领域"
       width="100"
       :filters="[
@@ -43,37 +42,30 @@
 export default {
   data() {
     return {
-      tableData: [
-        {
-          date: '2016-05-03',
-          name: 'Tom',
-          area: 'Biology',
-          money: '1000',
-          content: '985高校生命科学博士，丰富的留学履历和科研相关工作经验，擅长分子生物学领域',
-        },
-        {
-          date: '2016-05-02',
-          name: 'Alice',
-          area: 'Math',
-          money: '100',
-          content: '中科院数学研究员，主要研究兴趣为微分几何学，在国际指明会议发表多篇学术论文',
-        },
-        {
-          date: '2016-05-04',
-          name: 'Bob',
-          area: 'Physics',
-          money: '10',
-          content: '拥有20年从业经验的重点中学物理教师，擅长与学生发展良好的师生关系，课堂讲解深入浅出',
-        },
-        {
-          date: '2016-05-01',
-          name: 'Jack',
-          area: 'Chinese',
-          money: '1600',
-          content: '中国文学学士，长期从事中西方文化交流宣传活动，著有文集《清华的那段又冷又饿又困的时光》等',
-        },
-      ],
+      userlist: []
     }
+  },
+  created() {
+    fetch("/v1/user/filter?answerer=true", {
+      method: "GET",
+      headers: {"authorization": window.localStorage.getItem("token")},
+    })
+    .then(resp => {
+      if (!resp.ok) {
+        throw new Error("获取回答者列表失败")
+      }
+      return resp.json()
+    })
+    .then(data => {
+      this.userlist = data.userlist
+      console.log(data)
+    })
+    .catch(error => {
+      this.$message({
+        message: error,
+        type: "error",
+      })
+    })
   },
   methods: {
     formatter(row) {
