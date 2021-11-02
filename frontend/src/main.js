306a6a1d96a2ede39352ce96a8e9a914dcc475a1
Nick Cao <nickcao@nichi.co>
2021-11-02 16:21:49 +0800

    fix build

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,9 @@ import { createApp } from 'vue';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 import axios from 'axios';
+import Chat from 'vue3-beautiful-chat';
 import App from './App.vue';
 import router from './router';
-import Chat from 'vue3-beautiful-chat'
 
 const app = createApp(App);
 app.config.globalProperties.$http = axios;
