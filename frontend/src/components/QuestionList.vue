b200629a00c536a116d41ae2dd717304317cfd57
Nick Cao <nickcao@nichi.co>
2021-11-02 14:11:34 +0800

    chat: added nop chat to question list page

--- frontend/src/components/QuestionList.vue
+++ frontend/src/components/QuestionList.vue
@@ -1,4 +1,5 @@
 <template>
+  <el-container>
   <el-table
     ref="filterTable"
     :data="tableData"
@@ -8,6 +9,7 @@
     <el-table-column type="expand">
       <template #default="props">
         <p>{{ props.row.content }}</p>
+        <el-button type="primary" @click="openChat(props.row)">开始聊天</el-button>
       </template>
     </el-table-column>
     <el-table-column prop="date" label="日期" sortable min-width="20%" column-key="date" />
@@ -25,7 +27,7 @@
         { text: '别人问我的问题', value: 'que' },
       ]"
       :filter-method="filterTag"
-      filter-placement="bottom-end"
+      filter-placemeidnt="bottom-end"
     >
       <template #default="scope">
         <el-tag
@@ -36,18 +38,100 @@
       </template>
     </el-table-column>
   </el-table>
+  <beautiful-chat
+    style="z-index: 1000;"
+    :participants="participants"
+    :onMessageWasSent="onMessageWasSent"
+    :messageList="messageList"
+    :newMessagesCount="newMessagesCount"
+    :isOpen="isChatOpen"
+    :close="closeChat"
+    :showEmoji="false"
+    :open="() => {}"
+    :showFile="false"
+    :showEdition="false"
+    :showDeletion="false"
+    :showLauncher="false"
+    :showCloseButton="true"
+    :colors="colors"
+    :alwaysScrollToBottom="false"
+    :disableUserListToggle="true"
+    :messageStyling="true"
+    />
+  </el-container>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      participants: [
+      ],
+      messageList: [
+        { type: 'text', author: 'me', data: { text: 'Say yes!' } },
+        { type: 'text', author: 'user1', data: { text: 'No.' } },
+      ], // the list of the messages to show, can be paginated and adjusted dynamically
+      newMessagesCount: 0,
+      isChatOpen: false, // to determine whether the chat window should be open or closed
+      showTypingIndicator: '', // when set to a value matching the participant.id it shows the typing indicator for the specific user
+      colors: {
+        header: {
+          bg: '#4e8cff',
+          text: '#ffffff',
+        },
+        launcher: {
+          bg: '#4e8cff',
+        },
+        messageList: {
+          bg: '#ffffff',
+        },
+        sentMessage: {
+          bg: '#4e8cff',
+          text: '#ffffff',
+        },
+        receivedMessage: {
+          bg: '#eaeaea',
+          text: '#222222',
+        },
+        userInput: {
+          bg: '#f4f7f9',
+          text: '#565867',
+        },
+      },
       tableData: [],
     };
   },
   methods: {
     filterTag(value, row) {
       return row.tag === value;
+    },
+    openChat(row) {
+      console.log(row);
+      this.participants = [
+        {
+          id: row.answererid,
+          name: 'username', // TODO
+        },
+      ];
+      this.isChatOpen = true;
+      this.newMessagesCount = 0;
+    },
+    sendMessage(text) {
+      if (text.length > 0) {
+        this.newMessagesCount = this.isChatOpen ? this.newMessagesCount : this.newMessagesCount + 1;
+        this.onMessageWasSent({ author: 'support', type: 'text', data: { text } });
+      }
+    },
+    onMessageWasSent(message) {
+      // called when the user sends a message
+      this.messageList = [...this.messageList, message];
+    },
+    closeChat() {
+      this.isChatOpen = false;
+    },
+    handleScrollToTop() {
+      // called when the user scrolls message list to top
+      // leverage pagination for loading another page of messages
     },
   },
   created() {
