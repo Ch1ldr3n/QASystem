d00a5923666431a903d2458c23b1fea3c900c40b
cxjcxggg <chexj18@163.com>
2021-11-02 19:34:42 +0800

    #49 add admin question list

---
+++ frontend/src/components/AdminQuestion.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-main>
+    <el-container>
+      <el-aside width="unset">
+        <MyInfoCard />
+      </el-aside>
+      <el-container style="margin-left: 50px; margin-top: 20px">
+        <AdminQuestionList />
+      </el-container>
+    </el-container>
+  </el-main>
+</template>
+
+<script>
+import AdminQuestionList from './AdminQuestionList.vue'
+import MyInfoCard from './MyInfoCard.vue'
+
+export default {
+  name: 'question',
+  components: {
+    AdminQuestionList,
+    MyInfoCard,
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  },
+};
+</script>
