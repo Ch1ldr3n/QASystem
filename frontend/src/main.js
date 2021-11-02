f5b94e5600c112cea30314bbd434643115654725
Nick Cao <nickcao@nichi.co>
2021-11-02 11:44:00 +0800

    chat: embed into question detail page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,12 @@ import 'element-plus/dist/index.css';
 import axios from 'axios';
 import App from './App.vue';
 import router from './router';
+import Chat from 'vue3-beautiful-chat'
 
 const app = createApp(App);
 app.config.globalProperties.$http = axios;
 axios.defaults.baseURL = 'qanda-bauhinia.app.secoder.net/';
 app.use(ElementPlus);
 app.use(router);
+app.use(Chat);
 app.mount('#app');
