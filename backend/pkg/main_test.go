8a64f23c6b632c09199eebec284f6ad410a18198
Zhili <magiparty2300@126.com>
2021-11-08 08:42:32 +0800

    [#77] recover func TestQuestionX2

--- backend/pkg/main_test.go
+++ backend/pkg/main_test.go
@@ -626,6 +626,41 @@ func TestQuestionX1(t *testing.T) {
 	}
 }
 
+func TestQuestionX2(t *testing.T) {
+	e := GetEchoTestEnv("entQuestion")
+	token1, _ := GetIdTokenFromRec(AuxUserLogin(e, t, "user1", "pass"), t)
+	token2, userid2 := GetIdTokenFromRec(AuxUserLogin(e, t, "user2", "pass"), t)
+	token3, _ := GetIdTokenFromRec(AuxUserLogin(e, t, "user3", "pass"), t)
+	rec := AuxQuestionSubmit(e, t, token1, `
+{
+	"title": "test title6",
+	"content":"test content6",
+	"answererid":`+strconv.Itoa(userid2)+`
+}
+	`)
+	questionid6 := GetQuestionIdFromSubmit(rec, t)
+	AuxQuestionPay(e, t, questionid6, token1)
+
+	admintoken, _ := GetIdTokenFromRec(AuxAdminLogin(e, t, "admin", "admin"), t)
+	AuxQuestionReview(e, t, questionid6, true, admintoken)
+
+	// Accept: foreign interference
+	if rec := AuxQuestionAccept(e, nil, questionid6, true, token3); rec.Result().StatusCode != http.StatusBadRequest {
+		t.Fatal("question accept allows foreign interference")
+	}
+
+	// Close: foreign interference
+	AuxQuestionAccept(e, t, questionid6, true, token2)
+	if rec := AuxQuestionAccept(e, nil, questionid6, true, token3); rec.Result().StatusCode != http.StatusBadRequest {
+		t.Fatal("question close allows foreign interference")
+	}
+
+	// Cancel: foreign interference
+	if rec := AuxQuestionCancel(e, nil, questionid6, token3); rec.Result().StatusCode != http.StatusBadRequest {
+		t.Fatal("question cancel allows foreign interference")
+	}
+}
+
 // Query: inexistent question
 func TestQuestionQueryX1(t *testing.T) {
 	e := GetEchoTestEnv("entQuestion")
